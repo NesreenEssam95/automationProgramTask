2fea641efcff9560c92ad0843e7834687ebad089
NesreenEssam95 <155653647+NesreenEssam95@users.noreply.github.com>
2025-02-03 22:32:36 +0200

    Update BaseClass.java

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -26,8 +26,6 @@ public class BaseClass {
 		 WebDriverManager.chromedriver().setup();
 		 ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--headless");
-		 //options.addArguments("--disable-gpu");
-		 //options.addArguments("--no-sandbox");
 		 // Instantiate the chromeDriver
 		 driver = new ChromeDriver(options);
 		 wait =new WebDriverWait(driver, Duration.ofSeconds(10));
